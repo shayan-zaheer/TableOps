59da0461f295b5ec88c51d67543dc00d477fc336
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-09-27 03:00:10 +0500

    Delete Counter.jsx

--- client/src/components/Counter.jsx
+++
@@ -1,8 +0,0 @@
-function Counter() {
-    return (
-            <input placeholder="Enter quantity" type="number" min={1} maxLength={3} onClick={event => console.log(event)}
-            className="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-2.5 text-center" />
-    );
-}
-
-export default Counter;
